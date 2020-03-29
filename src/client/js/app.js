82171c11a9a8f2c694b6cc3f1dfa793e25312c3b
Philip <philipspan@gmail.com>
2020-03-29 14:41:48 -0700

    Added app.js

---
+++ src/client/js/app.js
@@ -0,0 +1,11 @@
+let inputLocation = document.getElementById("inputLocation");
+let submitLocation = document.getElementById("submitLocation");
+
+// Actions
+
+submitLocation.addEventListener("click", function() {
+
+		let userLocation = inputLocation.attribute.value;
+		console.log(userLocation);
+
+});
