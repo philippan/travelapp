47e685950fc063e6e76b305c99b092f6ca8f123e
Philip <philipspan@gmail.com>
2020-04-22 23:01:04 -0700

    Updated SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,14 @@ module.exports = {
           {
               test: /\.json$/,
               loader: 'json-loader'
+          },
+          {
+              test: /\.css$/i,
+              use: ['style-loader', 'css-loader'],
+          },
+          {
+              test: /\.s(a|c)ss$/,
+              use: ['style-loader', 'css-loader', 'sass-loader']
           }
       ],
     },
